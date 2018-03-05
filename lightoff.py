d935d90d711042b647d465c3b6ad8135d49ca241
Peijun Zhu <zpj.ustc@gmail.com>
2018-03-04 19:28:28 -0500

    space version

--- lightoff.py
+++ lightoff.py
@@ -1,4 +1,5 @@
 import numpy as np
+import itertools as it
 
 def GCD(a, b):
     m = np.array([1, 0])
@@ -104,49 +105,46 @@ def Ring(base, verbose=True):
         _verbose = verbose
     return _R
 
-@profile
-def linear_solver(m, im):
+
+def linear_solver(m, im, full=False):
     n = m.shape[0]
     im = im.reshape(n, -1)
     M = np.empty([n, n+im.shape[1]], dtype=m.dtype)
     M[:, :n] = m
     M[:, n:] = im
-    col2row = np.zeros([n], dtype='int')
-    row2col = np.zeros([n], dtype='int')
-    x, y = 0, 0
-    null = []
-    for y in range(n):
-        for i in range(x, n):
-            if M[i, y] != 0:
+    nul = set()
+    for x in range(n):
+        for i in it.chain(range(x, n), nul):
+            if M[i, x] != 0:
                 break
-        if M[i, y] == 0:
-            null.append(y)
-            row2col[y] = x - y - 1
+        if M[i, x] == 0:
+            nul.add(x)
             continue
         if x != i:
             M[[i, x]] = M[[x, i]]
 
-        p = 1/M[x, y]
+        p = 1/M[x, x]
         if p is None:
-            M[x, y:] /= M[x, y]
+            M[x, x:] /= M[x, x]
         else:
-            M[x, y:] *= p
+            M[x, x:] *= p
 
-        for i in range(x+1, n):
-            M[i, y:] -= M[x, y:]*M[i, y]
+        for i in it.chain(range(x+1, n), nul):
+            M[i, x:] -= M[x, x:]*M[i, x]
 
-        col2row[x] = y
-        row2col[y] = x
-        x += 1
-    col2row = col2row[:x]
-    for i in range(x-1, -1, -1):
-        y = col2row[i]
-        for j in range(i-1, -1, -1):
-            M[j, n:] -= M[i, n:]*M[j, y]
-            # Unnecessary operation
-            # M[j, y:n] = M[j, y:n] - M[i, y:n]*M[j, y]
-    M[list(range(n))] = M[row2col]
-    return M[:, n:], null
+    s = sorted(set(range(n)) - nul, reverse=True)
+    nul = sorted(nul)
+    for k, x in enumerate(s):
+        for i in s[k+1:]:
+            if full:
+                M[i, x:] -= M[x, x:]*M[i, x]
+            else:
+                M[i, n:] -= M[x, n:]*M[i, x]
+    if full:
+        return nul, M[:, n:], M[:, :n]
+    else:
+        return nul, M[:, n:]
+
 
 
 def invr(m):
@@ -186,31 +184,34 @@ def apply_sol(sol, s):
     if isinstance(s, str):
         s = str2mat(s)
     s = A2(s.flatten())
-    M, l = sol
-    criteria = np.einsum('ij, j->i', M[l], s)
+    nul, M = sol
+    criteria = np.einsum('ij, j->i', M[nul], s)
     if not all(criteria == 0):
         return None
     return np.einsum('ij, j->i', M, s).reshape(5, 5)
 
 def test_elimination():
     from fractions import Fraction
-    a = np.array([[1, 1, 1, 1], [0, 0, 1, 1], [0, 0, 3, 7], [0, 0, 2, 2]], dtype='double')
+    a = np.array([[1, 1, 1, 1], [0, 0, 1, 1], [0, 0, 3, 3], [0, 0, 2, 2]], dtype='double')
     a = np.vectorize(Fraction)(a)
-    m, n = invr(a)
-    print(m, n)
+    n, m = invr(a)
+    print("Null space", n)
+    print(m)
 
 def test_first_row():
-    import itertools as it
     solver = lightsoff_solver(5, 5)
     lights = np.zeros([25], dtype='int')
     row1 = ((0, 1),)*5
+    num = 0
     for elem in it.product(*row1):
         lights[:5] = elem
         sol = apply_sol(solver, lights)
         if sol is not None:
+            num += 1
             print('-'*30)
             print(lights.reshape(5, 5))
             print(sol)
+    assert num == 8, "Wrong number of solutions"
     return solver
 
 def interactive_solver():
@@ -225,6 +226,4 @@ def interactive_solver():
     print(apply_sol(solver, m))
 
 if __name__ == "__main__":
-    #test_first_row()
-    test_elimination()
-    #interactive_solver()
+    interactive_solver()
